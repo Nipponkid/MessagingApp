76951d348b0025a943297745313d713221082bf2
Wesley Brown <brown.5776@osu.edu>
2020-01-14 14:18:17 -0500

    Ensure Contacts Have a First Name

---
+++ src/MessagingApp.Domain/Contact.cs
@@ -0,0 +1,20 @@
+﻿namespace MessagingApp.Domain
+{
+    public struct Contact
+    {
+        private readonly string firstName;
+
+        public Contact(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+    }
+}
---
+++ tests/MessagingApp.Domain.Tests/ContactTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace MessagingApp.Domain.Tests
+{
+    public sealed class ContactTests
+    {
+        [Fact]
+        public void a_contact_has_a_first_name()
+        {
+            var contact = new Contact("John");
+            Assert.Equal("John", contact.FirstName);
+        }
+    }
+}
