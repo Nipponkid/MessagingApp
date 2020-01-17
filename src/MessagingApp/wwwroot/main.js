9c78af1beaec14fe323cceae9b060fcd805b47b4
Wesley Brown <brown.5776@osu.edu>
2020-01-17 14:44:28 -0500

    Fetch and Display Contacts on Contacts Page

---
+++ src/MessagingApp/wwwroot/main.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+const displayContacts = contacts => {
+    contacts.forEach(contact => {
+        const div = document.createElement('div');
+        const text = document.createTextNode(`${contact.firstName} ${contact.lastName}`);
+        div.appendChild(text);
+        document.body.appendChild(div);
+    });
+};
+
+fetch('api/contacts')
+    .then(response => response.json())
+    .then(json => {
+        displayContacts(json);
+    });
