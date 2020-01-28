9fc715b36b9cdf3de6ceb11f75056a6c840d8044
Wesley Brown <brown.5776@osu.edu>
2020-01-28 14:14:25 -0500

    Ensure a User has an ID
    Each user registered in the system should have a unique ID number.

---
+++ src/MessagingApp.Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MessagingApp.Domain
+{
+    public sealed class User
+    {
+        public User(long id)
+        {
+            Id = id;
+        }
+
+        public long Id { get; private set; }
+    }
+}
---
+++ tests/MessagingApp.Domain.Tests/UserTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace MessagingApp.Domain.Tests
+{
+    public sealed class UserTests
+    {
+        [Fact]
+        public void a_user_has_an_id()
+        {
+            var user = new User(0);
+            Assert.Equal(0, user.Id);
+        }
+    }
+}
