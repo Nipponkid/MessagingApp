d20550bca83aa6f89740beda9602ec2b1442cc93
Wesley Brown <brown.5776@osu.edu>
2020-01-29 12:56:52 -0500

    Create UsersService
    A UsersService will be responsible for manipulating the actual stored data
    for users.

---
+++ src/MessagingApp.Data/UsersService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using MessagingApp.Domain;
+
+namespace MessagingApp.Data
+{
+    public sealed class UsersService
+    {
+        public UsersService(List<User> users)
+        {
+            Users = users;
+        }
+
+        public IEnumerable<User> Users { get; private set; }
+    }
+}
---
+++ tests/MessagingApp.Data.Tests/UsersServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+using MessagingApp.Domain;
+
+namespace MessagingApp.Data.Tests
+{
+    public sealed class UsersServiceTests
+    {
+        [Fact]
+        public void a_users_service_has_a_list_of_all_users()
+        {
+            var users = new List<User>() {
+                new User(5, "user5@example.com"),
+                new User(123, "user123@example.com")
+            };
+            var service = new UsersService(users);
+            Assert.Equal(users, service.Users);
+        }
+    }
+}
