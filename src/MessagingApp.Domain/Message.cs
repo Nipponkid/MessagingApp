444d6841ab7468c0323c86feb051513b0b6aa9c2
Wesley Brown <wesleybrown@wesleybrown.net>
2020-02-12 18:54:28 -0500

    Ensure Messages Have Text
    Messages should have text that represents the content of the message.

---
+++ src/MessagingApp.Domain/Message.cs
@@ -0,0 +1,15 @@
+﻿namespace MessagingApp.Domain
+{
+    /// <summary>
+    /// A message.
+    /// </summary>
+    public sealed class Message
+    {
+        public Message(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; private set; }
+    }
+}
---
+++ tests/MessagingApp.Domain.Tests/MessageTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace MessagingApp.Domain.Tests
+{
+    public sealed class MessageTests
+    {
+        [Fact]
+        public void a_message_has_text()
+        {
+            var message = new Message("Hello, World!");
+            Assert.Equal("Hello, World!", message.Text);
+        }
+    }
+}
