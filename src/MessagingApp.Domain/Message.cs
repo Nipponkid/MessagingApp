414758bab5b5c8d7ff8a1765e1e5ec84bbba7bda
Wesley Brown <wesleybrown@wesleybrown.net>
2020-02-13 12:14:03 -0500

    Ensure Messages Have a Sender
    Every message should have a user who sent it.

--- src/MessagingApp.Domain/Message.cs
+++ src/MessagingApp.Domain/Message.cs
@@ -5,13 +5,15 @@
     /// </summary>
     public sealed class Message
     {
-        public Message(long id, string content)
+        public Message(long id, User sender, string content)
         {
             Id = id;
             Content = content;
+            Sender = sender;
         }
 
         public long Id { get; private set; }
         public string Content { get; private set; }
+        public User Sender { get; private set; }
     }
 }
--- tests/MessagingApp.Domain.Tests/MessageTests.cs
+++ tests/MessagingApp.Domain.Tests/MessageTests.cs
@@ -4,17 +4,25 @@ namespace MessagingApp.Domain.Tests
 {
     public sealed class MessageTests
     {
+        private readonly User sender;
         private readonly Message message;
 
         public MessageTests()
         {
-            message = new Message(1, "Hello, World!");
+            sender = new User(1, "user1@example.com");
+            message = new Message(1, sender, "Hello, World!");
         }
 
         [Fact]
         public void a_message_has_an_id()
         {
             Assert.Equal(1, message.Id);
+        }
+
+        [Fact]
+        public void a_message_has_a_sender()
+        {
+            Assert.Equal(sender, message.Sender);
         }
 
         [Fact]
