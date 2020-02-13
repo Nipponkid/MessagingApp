253034d8d9dcf8c152f7b48c0f85ee462605987c
Wesley Brown <wesleybrown@wesleybrown.net>
2020-02-13 14:06:35 -0500

    Ensure Getting All Messages Returns 200 OK
    Getting all messages should return 200 OK regardless of how many messages
    there actually are to return.

---
+++ src/MessagingApp/Messages/MessagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessagingApp.Messages
+{
+    public sealed class MessagesController : ControllerBase
+    {
+        public IActionResult GetAllMessages()
+        {
+            return Ok(null);
+        }
+    }
+}
---
+++ tests/MessagingApp.Tests/MessagesControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+using MessagingApp.Messages;
+
+namespace MessagingApp.Tests
+{
+    public sealed class MessagesControllerTests
+    {
+        [Fact]
+        public void getting_all_messages_returns_200_OK()
+        {
+            var messagesController = new MessagesController();
+            var result = messagesController.GetAllMessages();
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
