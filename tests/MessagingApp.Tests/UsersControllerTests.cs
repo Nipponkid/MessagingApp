c49af14b4f41734055ded0e63bbfa08a6624e384
Wesley Brown <wesleybrown@wesleybrown.net>
2020-02-18 13:32:38 -0500

    Capitalize First Letter of All UsersControllerTests

--- tests/MessagingApp.Tests/UsersControllerTests.cs
+++ tests/MessagingApp.Tests/UsersControllerTests.cs
@@ -38,27 +38,27 @@ namespace MessagingApp.Tests
         }
 
         [Fact]
-        public void a_users_controller_has_a_list_of_all_users()
+        public void A_users_controller_has_a_list_of_all_users()
         {
             var result = controller.GetAllUsers() as OkObjectResult;
             Assert.Equal(users, result.Value);
         }
 
         [Fact]
-        public void getting_all_users_returns_a_200_OK()
+        public void Getting_all_users_returns_a_200_OK()
         {
             Assert.IsType<OkObjectResult>(controller.GetAllUsers());
         }
 
         [Fact]
-        public void getting_a_user_by_a_valid_id_returns_200_OK()
+        public void Getting_a_user_by_a_valid_id_returns_200_OK()
         {
             var id = GetAUser().Id;
             Assert.IsType<OkObjectResult>(controller.GetUserById(id));
         }
 
         [Fact]
-        public void getting_a_user_by_a_valid_id_returns_that_user()
+        public void Getting_a_user_by_a_valid_id_returns_that_user()
         {
             var user = GetAUser();
             var response = controller.GetUserById(user.Id) as OkObjectResult;
@@ -66,7 +66,7 @@ namespace MessagingApp.Tests
         }
 
         [Fact]
-        public void posting_a_valid_user_returns_201_Created_with_Location_header()
+        public void Posting_a_valid_user_returns_201_Created_with_Location_header()
         {
             var userQuery = new UserQuery(2, "user2@example.com");
             var response = controller.PostUser(userQuery);
@@ -74,7 +74,7 @@ namespace MessagingApp.Tests
         }
 
         [Fact]
-        public void posting_a_user_adds_that_user_to_the_list_of_all_users()
+        public void Posting_a_user_adds_that_user_to_the_list_of_all_users()
         {
             var userQuery = new UserQuery(7, "user7@example.com");
             controller.PostUser(userQuery);
@@ -83,7 +83,7 @@ namespace MessagingApp.Tests
         }
 
         [Fact]
-        public void deleting_an_existing_user_returns_200_OK()
+        public void Deleting_an_existing_user_returns_200_OK()
         {
             var userToDelete = GetAUser();
             var response = controller.DeleteUserById(userToDelete.Id);
@@ -91,7 +91,7 @@ namespace MessagingApp.Tests
         }
 
         [Fact]
-        public void deleting_an_existing_user_returns_that_user()
+        public void Deleting_an_existing_user_returns_that_user()
         {
             var userToDelete = GetAUser();
             var response = controller.DeleteUserById(userToDelete.Id) as OkObjectResult;
@@ -100,7 +100,7 @@ namespace MessagingApp.Tests
         }
 
         [Fact]
-        public void deleting_an_existing_user_removes_that_user_from_the_list_of_all_users()
+        public void Deleting_an_existing_user_removes_that_user_from_the_list_of_all_users()
         {
             var userToDelete = GetAUser();
             controller.DeleteUserById(userToDelete.Id);
