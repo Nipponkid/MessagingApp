1a2e99c87150115d16ec04930e66130a12f1f45f
Wesley Brown <wesleybrown@wesleybrown.net>
2020-02-19 12:39:07 -0500

    Ensure Adding a Message Returns That Message
    Adding a message to a messages service should return that message.

---
+++ src/MessagingApp.Data/Messages/MessagesService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using MessagingApp.Domain;
+
+namespace MessagingApp.Data.Messages
+{
+    public sealed class MessagesService
+    {
+        public MessagesService(List<Message> messages)
+        { }
+
+        public Message AddMessage(Message message)
+        {
+            return message;
+        }
+    }
+}
---
+++ tests/MessagingApp.Data.Tests/MessagesServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+using MessagingApp.Data.Messages;
+using MessagingApp.Domain;
+
+namespace MessagingApp.Data.Tests
+{
+    public sealed class MessagesServiceTests
+    {
+        [Fact]
+        public void Adding_a_message_returns_that_message()
+        {
+            var sender = new User(1, "user1@example.com");
+            var receiver = new User(2, "user2@example.com");
+            var messageToAdd = new Message(1, sender, receiver, "What's up?");
+            var messagesService = new MessagesService(new List<Message>());
+            var addedMessage = messagesService.AddMessage(messageToAdd);
+            Assert.Equal(messageToAdd, addedMessage);
+        }
+    }
+}
