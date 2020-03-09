f961f0adfc5c33fec8e48d953d647c3b3a2c9496
Wesley Brown <wesleybrown@wesleybrown.net>
2020-03-09 16:44:32 -0400

    Refactor run.sh
    Refactor the run.sh script to create a MessagingApp docker container if one
    doesn't exist.

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Start docker container
+if ! docker start MessagingApp1; then 
+    # Container doesn't exist so create it
+    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Your_password123" \
+        -p 1433:1433 --name MessagingApp1 \
+        -d mcr.microsoft.com/mssql/server:2017-latest
+fi
+
+# Run the MessagingApp server
 cd src/MessagingApp
 dotnet run
 
